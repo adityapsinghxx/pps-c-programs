1ff0ebfaad42d8575b3abe4847290c60b7fe06fb
Aditya Pratap Singh <itsadityapsingh@gmail.com>
2026-01-06 01:05:49 +0530

    Create typecasting.c

---
+++ typecasting.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 2;
+    float result;
+
+    result = (float)a / b; //manual conversion of one data type into another//
+    printf("Result = %f", result);
+
+    return 0;
+}
