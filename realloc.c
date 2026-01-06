7983572e2d33d6f9ad79fdc823f56d8d44387bbc
Aditya Pratap Singh <itsadityapsingh@gmail.com>
2026-01-06 13:19:24 +0530

    Create realloc.c

---
+++ realloc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *p;
+    int i;
+
+    p = (int*) malloc(3 * sizeof(int));
+
+    for(i = 0; i < 3; i++) {
+        p[i] = i + 1;
+    }
+
+    p = realloc(p, 5 * sizeof(int));
+
+    for(i = 3; i < 5; i++) {
+        p[i] = i + 1;
+    }
+
+    for(i = 0; i < 5; i++) {
+        printf("%d ", p[i]);
+    }
+
+    free(p);
+    return 0;
+}
